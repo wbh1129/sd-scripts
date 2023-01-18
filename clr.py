72666834c05bb07fcb23fb334a6a65217d171982
wbh1129 <williambhard@proton.me>
2023-01-18 00:22:14 -0600

    clr.py: implement CLI

--- clr.py
+++ clr.py
@@ -1,34 +1,39 @@
 #!/usr/bin/env python3
+# Precomputed learning schedules can be found in the clr directory
 
 from math import *
+import argparse
 
-# The recommended values are just based on things I read online. Please feel 
-# free to experiment, let me know if you find anything interesting!
-
-MAX=2e-4
-MIN=MAX/6 # recommended value is MAX/6
-CYCLE_LEN=10 # cycle length, recommended (2-10) * steps_per_epoch
-CYCLES=50 # number of cycles to generate
-DECAY=0.0 # lower numbers decay slower, higher numbers decay faster, 0 disables decay
-RESTARTS=1 # 1 for half wave, 0 for full wave (CYCLE_LEN is doubled)
-START=2 # 2 for single full wave prior to half waves, 1 to start at MAX, 0 to start at MIN
-ONECYCLE=0 # 1cycle phase length, 0 to disable
-ONECYCLE_MIN=MIN/100
-STEP_OFF=0 # step offset, for resuming training with a different schedule
-WAVE=1 # 1 for sine, 0 for triangle/sawtooth
-PLOT=0 # plot out the learning rate schedule w/ matplotlib
+DEFAULT_MAX=2e-4
+parser = argparse.ArgumentParser(
+        description="cyclical learning rates for Auto1111 training",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+)
+parser.add_argument("--max", default=DEFAULT_MAX, help="cycle peak value")
+parser.add_argument("--min", default=DEFAULT_MAX/6, help="cycle minimum value, recommended max/6")
+parser.add_argument("--len", default=10, help="cycle length, recommended (2-10) * steps_per_epoch")
+parser.add_argument("--cycles", default=50, help="quantity of cycles to generate")
+parser.add_argument("--decay", default=0.0, help="rate of decay")
+parser.add_argument("--no-restarts", action="store_true", help="do full waves instead of half")
+parser.add_argument("--start", default=2, help="wave start point; 0 for min; 1 for max; 2 for full wave followed by max")
+parser.add_argument("--onecycle", default=0, help="1cycle phase length, 0 to disable")
+parser.add_argument("--onecycle-min", default=DEFAULT_MAX/100, help="1cycle phase minimum")
+parser.add_argument("--step-start", default=0, help="starting point for steps, useful for resuming training w/ a new schedule")
+parser.add_argument("--wave", default=1, help="0 for triangle; 1 for sine")
+parser.add_argument("--plot", action="store_true", help="show a plot of the LR schedule w/ matplotlib")
+ARGS = parser.parse_args()
 
 # TODO add variable length cycles
 
-if ONECYCLE > 0:
-    CYCLES=1
-    START=0
-    RESTARTS=0
+if ARGS.onecycle > 0:
+    ARGS.cycles=1
+    ARGS.start=0
+    ARGS.no_restarts = True
 
 def transform_wave(func):
     def wrap(*args, **kwargs):
         val = func(*args, **kwargs)
-        if WAVE == 1:
+        if ARGS.wave == 1:
             return sin(val * pi/2)
         else:
             return val
@@ -44,33 +49,33 @@ def tri(start, i, c_len):
 
 rates = []
 
-if START == 2:
-    firstwave = lambda i: tri(0, i, ceil(CYCLE_LEN/2))
-    rates = [MIN * (1 - firstwave(i)) + MAX * firstwave(i) for i in range(CYCLE_LEN-1)] + rates;
-    START=1
+if ARGS.start == 2:
+    firstwave = lambda i: tri(0, i, ceil(ARGS.len/2))
+    rates = [ARGS.min * (1 - firstwave(i)) + ARGS.max * firstwave(i) for i in range(ARGS.len-1)] + rates;
+    ARGS.start=1
 
-if RESTARTS == 1:
-    FULL_CYCLE_LEN=CYCLE_LEN
-    STEPS=CYCLE_LEN*CYCLES
-    wave = lambda i: saw(START, i, CYCLE_LEN)
+if ARGS.no_restarts:
+    FULL_CYCLE_LEN=2*(ARGS.len-1)
+    STEPS=FULL_CYCLE_LEN*ARGS.cycles+1
+    wave = lambda i: tri(ARGS.start, i, ARGS.len)
 else:
-    FULL_CYCLE_LEN=2*(CYCLE_LEN-1)
-    STEPS=FULL_CYCLE_LEN*CYCLES+1
-    wave = lambda i: tri(START, i, CYCLE_LEN)
+    FULL_CYCLE_LEN=ARGS.len
+    STEPS=ARGS.len*ARGS.cycles
+    wave = lambda i: saw(ARGS.start, i, ARGS.len)
 
-decay = lambda v,i: v / (1 + i//FULL_CYCLE_LEN * DECAY)
+decay = lambda v,i: v / (1 + i//FULL_CYCLE_LEN * ARGS.decay)
 
-rates += [decay(MIN,i) * (1 - wave(i)) + decay(MAX,i) * wave(i) for i in range(STEPS)]
+rates += [decay(ARGS.min,i) * (1 - wave(i)) + decay(ARGS.max,i) * wave(i) for i in range(STEPS)]
 
-if ONECYCLE > 0:
-    wave_1cyc = lambda i: tri(0, i, ONECYCLE+1)
-    rates += [MIN * (1 - wave_1cyc(i)) + ONECYCLE_MIN * wave_1cyc(i) for i in range(1, ONECYCLE+1)]
+if ARGS.onecycle > 0:
+    wave_1cyc = lambda i: tri(0, i, ARGS.onecycle+1)
+    rates += [ARGS.min * (1 - wave_1cyc(i)) + ARGS.onecycle_min * wave_1cyc(i) for i in range(1, ARGS.onecycle+1)]
 
-rates_str = [f"{x:.2e}:{i+1+STEP_OFF}" for (i,x) in enumerate(rates)]
+rates_str = [f"{x:.2e}:{i+1+ARGS.step_start}" for (i,x) in enumerate(rates)]
 rates_str[-1] = rates_str[-1].split(":")[0] # coast on final LR
 print(", ".join(rates_str))
 
-if PLOT == 1:
+if ARGS.plot:
     import matplotlib.pyplot as plt
     plt.plot(range(1, len(rates)+1), rates)
     plt.grid()
