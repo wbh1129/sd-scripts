122b7f48dd5f3d3d171fac15e8be67bd0fe6b580
wbh1129 <williambhard@proton.me>
2023-03-03 08:46:33 -0600

    plot_ti.py: avg3 instead of avg5

--- plot_ti.py
+++ plot_ti.py
@@ -12,7 +12,7 @@ avg      = lambda arr: sum(arr)/len(arr)
 avg_loss = lambda r: [avg(csv["loss"][max(i-r,0):i+r+1])
                       for i in range(len(csv["loss"]))]
 
-loss.plot(csv["step"], avg_loss(5), "r", linewidth=2, label="avg5")
+loss.plot(csv["step"], avg_loss(3), "r", linewidth=2, label="avg3")
 loss.plot(csv["step"], avg_loss(10), "b", linewidth=2, label="avg10")
 loss.set_title(f"Min: {min(csv['loss'])}, Max: {max(csv['loss'])}, Avg: {avg(csv['loss'])}")
 loss.legend()
